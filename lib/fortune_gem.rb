3dea4de7d156cb822577cb16022a57d76311d038
Stephen Schor <stephen@ubuntbox.(none)>
2011-05-01 23:54:26 -0400

    giving users the ability to limit the characters of the fortune they want

--- lib/fortune_gem.rb
+++ lib/fortune_gem.rb
@@ -1,8 +1,19 @@
 module FortuneGem
   
-  def self.give_fortune
-    fortunes ||= File.open("#{File.dirname(__FILE__)}/fortunes").read.split("%")
-    fortunes[rand(fortunes.length)].sub("\n", "")
+  FORTUNES = File.open("#{File.dirname(__FILE__)}/fortunes").read.split("%").map{|f| f.sub("\n", "").strip }
+  
+  # Pass an option of :max_length if you want to limit length of fortunes                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       # 
+  def self.give_fortune(options = {:max_length => nil})
+    fortune = nil
+    
+    if options[:max_length]
+      short_listed = FORTUNES.find_all{|f| f.length <= options[:max_length].to_i}
+      fortune = short_listed[rand(short_listed.length)]
+    else
+      fortune = FORTUNES[rand(FORTUNES.length)]
+    end
+
+    fortune
   end
   
 end
