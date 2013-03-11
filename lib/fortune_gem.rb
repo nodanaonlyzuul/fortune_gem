4056d691304a0e8d7fc6c8ef683ee73164404d71
nodanaonlyzuul <beholdthepanda@gmail.com>
2013-03-11 09:33:06 -0400

    refactoring

--- lib/fortune_gem.rb
+++ lib/fortune_gem.rb
@@ -1,10 +1,15 @@
-module FortuneGem
+class FortuneGem
   
   FORTUNES = File.open("#{File.dirname(__FILE__)}/fortunes").read.split("%").map{|f| f.sub("\n", "").strip }
+
+  def initialize(options = {:max_length => nil})
+    
+  end
   
-  # Pass an option of :max_length if you want to limit length of fortunes                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       # 
-  def self.give_fortune(options = {:max_length => nil})
-    fortune = nil
+  # Pass an option of :max_length if you want to limit length of fortunes                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       #
+  def self.give_fortune(options)
+    instance  = self.new(options)
+    fortune   = nil
     
     if options[:max_length]
       short_listed = FORTUNES.find_all{|f| f.length <= options[:max_length].to_i}
@@ -12,7 +17,7 @@ module FortuneGem
     else
       fortune = FORTUNES[rand(FORTUNES.length)]
     end
-
+    
     fortune
   end
   
