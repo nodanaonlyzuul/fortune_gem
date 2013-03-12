577e7091afe2efa093257d4ae6b8b394fe6b5bf6
Stephen Schor <stephen@eastmedia.com>
2013-03-12 10:43:04 -0400

    Move the variable holding the path to fortune files from a contant to a class level getter and setter

--- lib/fortune_gem.rb
+++ lib/fortune_gem.rb
@@ -1,24 +1,26 @@
 class FortuneGem
-  
-  FORTUNES = File.open("#{File.dirname(__FILE__)}/fortunes").read.split("%").map{|f| f.sub("\n", "").strip }
 
-  def initialize(options = {:max_length => nil})
-    
-  end
-  
   # Pass an option of :max_length if you want to limit length of fortunes                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       #
-  def self.give_fortune(options)
+  def self.give_fortune(options = {})
     instance  = self.new(options)
     fortune   = nil
-    
+
     if options[:max_length]
-      short_listed = FORTUNES.find_all{|f| f.length <= options[:max_length].to_i}
+      short_listed = fortunes.find_all{|f| f.length <= options[:max_length].to_i}
       fortune = short_listed[rand(short_listed.length)]
     else
-      fortune = FORTUNES[rand(FORTUNES.length)]
+      fortune = fortunes[rand(fortunes.length)]
     end
-    
+
     fortune
   end
-  
+
+  def self.fortunes
+    @fortunes ||= File.open("#{File.dirname(__FILE__)}/fortunes").read.split("%").map{|f| f.sub("\n", "").strip }
+  end
+
+  def self.fortunes=(path)
+    @fortunes = path
+  end
+
 end
